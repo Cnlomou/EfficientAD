d97a7d33e42feb40acd0c868d21296c45799cd42
unknown <cnlomou@0430442743e448c.0430442743e448c>
2024-04-28 10:18:35 +0800

    backup

---
+++ common.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os.path
+
+from PIL import Image
+from torch import nn
+from torchvision.datasets import ImageFolder
+
+def get_autoencoder(out_channels=384):
+    return nn.Sequential(
+        # encoder
+        nn.Conv2d(in_channels=3, out_channels=32, kernel_size=4, stride=2,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=4, stride=2,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=2,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=2,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=8),
+        # decoder
+        nn.Upsample(size=3, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=8, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=15, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=32, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=63, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=127, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
+                  padding=2),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.2),
+        nn.Upsample(size=56, mode='bilinear'),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,
+                  padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=3,
+                  stride=1, padding=1)
+    )
+
+def get_pdn_small(out_channels=384, padding=False):
+    pad_mult = 1 if padding else 0
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4,
+                  padding=3 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.AvgPool2d(kernel_size=2, stride=2, padding=1 * pad_mult),
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4,
+                  padding=3 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.AvgPool2d(kernel_size=2, stride=2, padding=1 * pad_mult),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3,
+                  padding=1 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=256, out_channels=out_channels, kernel_size=4)
+    )
+
+def get_pdn_medium(out_channels=384, padding=False):
+    pad_mult = 1 if padding else 0
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=256, kernel_size=4,
+                  padding=3 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.AvgPool2d(kernel_size=2, stride=2, padding=1 * pad_mult),
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4,
+                  padding=3 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.AvgPool2d(kernel_size=2, stride=2, padding=1 * pad_mult),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3,
+                  padding=1 * pad_mult),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=512, out_channels=out_channels, kernel_size=4),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
+                  kernel_size=1)
+    )
+
+class ImageFolderWithoutTarget(ImageFolder):
+    def __getitem__(self, index):
+        sample, target = super().__getitem__(index)
+        return sample
+
+class ImageFolderWithPath(ImageFolder):
+    def __init__(self, root, size, transform=None, target_transform=None):
+        super(ImageFolderWithPath, self).__init__(root, transform, target_transform)
+        self.size = size
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        image = self.loader(path)
+        sample_class = 0
+        if "good" in path:
+            target = Image.new('L', (self.size, self.size))
+        else:
+            t = path.split('\\')
+            t[-3] = 'ground_truth'
+            t[-1] = t[-1].replace('.', '_mask.')
+            target = self.loader("\\".join(t))
+            sample_class = 1
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return (image, target[:1]), sample_class, path
+
+def InfiniteDataloader(loader):
+    iterator = iter(loader)
+    while True:
+        try:
+            yield next(iterator)
+        except StopIteration:
+            iterator = iter(loader)
